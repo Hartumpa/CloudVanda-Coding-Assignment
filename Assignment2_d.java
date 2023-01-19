60820294d2685535c45c7198cfb6e0c7c0089cac
Hartumpa <iambeastonn@gmail.com>
2023-01-20 03:07:29 +0530

    mostly done

---
+++ Assignment2_d.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assignment2_d {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Sentence: ");
+        String sentence = scanner.nextLine();
+        System.out.print("Enter a Letter: ");
+        String letter = scanner.nextLine();
+        int index = sentence.indexOf(letter);
+        if (index == -1) {
+            System.out.println("The letter does not exist in the sentence.");
+        } else {
+            System.out.println("The rest of the sentence after the first occurrence of the letter: " + sentence.substring(index + 1));
+        }
+    }
+}
